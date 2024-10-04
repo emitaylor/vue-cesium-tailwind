2abfac4dfd457e1cbf67fe3135cdf1134cc409e6
Emi Taylor <letailleur.emilie@gmail.com>
2024-10-04 22:54:14 +0200

    good night

--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -24,7 +24,7 @@
 
 <script>
 import { onMounted, ref } from 'vue'
-import { Viewer, Cartesian3, HeadingPitchRoll, Transforms, Color, Math as CesiumMath, SampledPositionProperty, JulianDate } from 'cesium'
+import { Viewer, Cartesian3, HeadingPitchRoll, Transforms, Color, Math as CesiumMath } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 export default {
@@ -33,52 +33,59 @@ export default {
     const viewerContainer = ref(null)
     const viewer = ref(null)
     const droneEntity = ref(null)
-    // const visionConeEntity = ref(null)
     const dronePath = ref([])
     const currentIndex = ref(0)
     const timer = ref(null)
 
-    // drone position
-    let currentPosition = Cartesian3.fromDegrees(2.3522, 48.8566, 100)
-    let currentOrientation = Transforms.headingPitchRollQuaternion(
-      currentPosition,
-      new HeadingPitchRoll(
-        CesiumMath.toRadians(0),
-        CesiumMath.toRadians(0),
-        CesiumMath.toRadians(0)
-      )
-    );
-
     const fetchDronePath = async () => {
       try {
         const response = await fetch('/datas.json')
-          .then(response => response.json())
-          .then(data => {
-            console.log('Données JSON reçues:', data)
-            const flightRecords = data.flight_records
-            if (flightRecords) {
-              console.log('%c flightRecords HERE!', 'background: #222; color: #bada55')
-              console.log(flightRecords)
-              console.log('%c END', 'background: #222; color: #bada55')
+        const data = await response.json()
+        console.log('Données JSON reçues:', data)
+        const flightRecords = data.flight_records
+        if (flightRecords) {
+          console.log('%c flightRecords HERE!', 'background: #222; color: #bada55')
+          console.log(flightRecords)
+          console.log('%c END', 'background: #222; color: #bada55')
+
+          Object.keys(flightRecords).forEach(recordKey => {
+            const record = flightRecords[recordKey]
+            console.log('Données de l\'enregistrement:', record)
+
+            if (record.latitude != null && record.longitude != null) {
+              dronePath.value.push({
+                latitude: record.latitude,
+                longitude: record.longitude,
+                altitude: record.height || 0,
+                attitude_head: record.attitude_head,
+                attitude_pitch: record.attitude_pitch,
+                attitude_roll: record.attitude_roll,
+                timestamp: recordKey
+              })
+            } else {
+              console.warn(`L'enregistrement pour l'horodatage ${recordKey} ne contient pas de latitude ou de longitude valides.`)
             }
           })
+        } else {
+          console.error('Aucun enregistrement de vol trouvé dans les données.')
+        }
       } catch (error) {
         console.error('Erreur lors du chargement du fichier JSON', error)
       }
     }
-    onMounted(async () => {
-      await fetchDronePath()
 
+    onMounted(async () => {
       viewer.value = new Viewer(viewerContainer.value, {
         shouldAnimate: true,
         fullscreenButton: true,
         timeline: false,
         infoBox: false,
-        selectionIndicator: false,
-      });
+        selectionIndicator: false
+      })
 
-      // Initialiser le drone si les données JSON ont été chargées
-      if (dronePath.value.length > 0) {
+      await fetchDronePath()
+
+      if (viewer.value && dronePath.value.length > 0) {
         const initialData = dronePath.value[0]
         const initialPosition = Cartesian3.fromDegrees(initialData.longitude, initialData.latitude, initialData.altitude)
 
@@ -87,9 +94,9 @@ export default {
           position: initialPosition,
           point: {
             pixelSize: 10,
-            color: Color.RED,
-          },
-        });
+            color: Color.RED
+          }
+        })
 
         viewer.value.zoomTo(droneEntity.value)
       } else {
@@ -98,7 +105,7 @@ export default {
     })
 
     const playFlight = () => {
-      console.log('playFlight called');
+      console.log('playFlight called')
       if (currentIndex.value >= dronePath.value.length - 1) {
         currentIndex.value = 0
       }
@@ -110,8 +117,8 @@ export default {
         } else {
           clearInterval(timer.value)
         }
-      }, 1000);
-    };
+      }, 1000)
+    }
 
     const pauseFlight = () => {
       console.log('pauseFlight called')
@@ -119,7 +126,7 @@ export default {
         clearInterval(timer.value)
         timer.value = null
       }
-    };
+    }
 
     const nextStep = () => {
       console.log('nextStep called')
@@ -127,7 +134,7 @@ export default {
         currentIndex.value++
         updateDronePosition(dronePath.value[currentIndex.value])
       }
-    };
+    }
 
     const prevStep = () => {
       console.log('prevStep called')
@@ -135,26 +142,25 @@ export default {
         currentIndex.value--
         updateDronePosition(dronePath.value[currentIndex.value])
       }
-    };
+    }
 
     const jumpToStep = (index) => {
       console.log('jumpToStep called with index:', index)
       currentIndex.value = index
       updateDronePosition(dronePath.value[currentIndex.value])
-    };
+    }
 
     const updateDronePosition = (data) => {
       console.log('updateDronePosition called with data:', data)
 
       if (!droneEntity.value) {
         console.error("droneEntity is not défini.")
-        return;
+        return
       }
 
       const position = Cartesian3.fromDegrees(data.longitude, data.latitude, data.altitude)
       droneEntity.value.position = position
 
-      // Mettre à jour l'orientation du drone
       const orientation = Transforms.headingPitchRollQuaternion(
         position,
         new HeadingPitchRoll(
@@ -162,11 +168,10 @@ export default {
           CesiumMath.toRadians(data.attitude_pitch),
           CesiumMath.toRadians(data.attitude_roll)
         )
-      );
+      )
 
       droneEntity.value.orientation = orientation
-    };
-
+    }
 
     return {
       viewerContainer,
@@ -175,23 +180,23 @@ export default {
       pauseFlight,
       nextStep,
       prevStep,
-      jumpToStep,
-    };
-  },
-};
+      jumpToStep
+    }
+  }
+}
 </script>
 
 <style scoped>
 .app-container {
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: center
 }
 
 .controls {
   margin-bottom: 20px;
   display: flex;
-  gap: 10px;
+  gap: 10px
 }
 
 .control-button {
@@ -201,19 +206,19 @@ export default {
   padding: 10px 20px;
   border-radius: 5px;
   cursor: pointer;
-  font-size: 16px;
+  font-size: 16px
 }
 
 .control-button:hover {
-  background-color: #0056b3;
+  background-color: #0056b3
 }
 
 .cesium-viewer-container {
   width: 100vw;
-  height: 60vh;
+  height: 60vh
 }
 
 .event-list {
-  margin-top: 20px;
+  margin-top: 20px
 }
 </style>
