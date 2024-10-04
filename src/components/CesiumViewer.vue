cd12325ae41c014b8dbb784e5062064a207067df
Emi Taylor <letailleur.emilie@gmail.com>
2024-10-04 22:27:20 +0200

    fix JSON datas

--- src/components/CesiumViewer.vue
+++ src/components/CesiumViewer.vue
@@ -24,22 +24,22 @@
 
 <script>
 import { onMounted, ref } from 'vue'
-import { Viewer, Cartesian3, HeadingPitchRoll, Transforms, Color, Math as CesiumMath, SampledPositionProperty, JulianDate  } from 'cesium'
+import { Viewer, Cartesian3, HeadingPitchRoll, Transforms, Color, Math as CesiumMath, SampledPositionProperty, JulianDate } from 'cesium'
 import 'cesium/Build/Cesium/Widgets/widgets.css'
 
 export default {
   name: 'CesiumViewer',
   setup() {
-    const viewerContainer = ref(null);
-    const viewer = ref(null);
-    const droneEntity = ref(null);
-    const visionConeEntity = ref(null);
-    const dronePath = ref([]);
-    const currentIndex = ref(0);
-    const timer = ref(null);
+    const viewerContainer = ref(null)
+    const viewer = ref(null)
+    const droneEntity = ref(null)
+    // const visionConeEntity = ref(null)
+    const dronePath = ref([])
+    const currentIndex = ref(0)
+    const timer = ref(null)
 
     // drone position
-    let currentPosition = Cartesian3.fromDegrees(2.3522, 48.8566, 100);
+    let currentPosition = Cartesian3.fromDegrees(2.3522, 48.8566, 100)
     let currentOrientation = Transforms.headingPitchRollQuaternion(
       currentPosition,
       new HeadingPitchRoll(
@@ -51,114 +51,121 @@ export default {
 
     const fetchDronePath = async () => {
       try {
-        const response = await fetch('/datas.json');
-        dronePath.value = await response.json();
-        console.log('dronePath loaded:', dronePath.value);
+        const response = await fetch('/datas.json')
+          .then(response => response.json())
+          .then(data => {
+            console.log('Données JSON reçues:', data)
+            const flightRecords = data.flight_records
+            if (flightRecords) {
+              console.log('%c flightRecords HERE!', 'background: #222; color: #bada55')
+              console.log(flightRecords)
+              console.log('%c END', 'background: #222; color: #bada55')
+            }
+          })
       } catch (error) {
-        console.error('Erreur lors du chargement du fichier JSON:', error);
+        console.error('Erreur lors du chargement du fichier JSON', error)
       }
+    }
+    onMounted(async () => {
+      await fetchDronePath()
+
+      viewer.value = new Viewer(viewerContainer.value, {
+        shouldAnimate: true,
+        fullscreenButton: true,
+        timeline: false,
+        infoBox: false,
+        selectionIndicator: false,
+      });
+
+      // Initialiser le drone si les données JSON ont été chargées
+      if (dronePath.value.length > 0) {
+        const initialData = dronePath.value[0]
+        const initialPosition = Cartesian3.fromDegrees(initialData.longitude, initialData.latitude, initialData.altitude)
+
+        droneEntity.value = viewer.value.entities.add({
+          name: 'Drone',
+          position: initialPosition,
+          point: {
+            pixelSize: 10,
+            color: Color.RED,
+          },
+        });
+
+        viewer.value.zoomTo(droneEntity.value)
+      } else {
+        console.error("Les données JSON ne contiennent aucune position de drone valide.")
+      }
+    })
+
+    const playFlight = () => {
+      console.log('playFlight called');
+      if (currentIndex.value >= dronePath.value.length - 1) {
+        currentIndex.value = 0
+      }
+
+      timer.value = setInterval(() => {
+        if (currentIndex.value < dronePath.value.length) {
+          updateDronePosition(dronePath.value[currentIndex.value])
+          currentIndex.value++
+        } else {
+          clearInterval(timer.value)
+        }
+      }, 1000);
     };
 
-    onMounted(async () => {
-  await fetchDronePath();
-
-  viewer.value = new Viewer(viewerContainer.value, {
-    shouldAnimate: true,
-    fullscreenButton: true,
-    timeline: false,
-    infoBox: false,
-    selectionIndicator: false,
-  });
-
-  // Initialiser le drone si les données JSON ont été chargées
-  if (dronePath.value.length > 0) {
-    const initialData = dronePath.value[0];
-    const initialPosition = Cartesian3.fromDegrees(initialData.longitude, initialData.latitude, initialData.altitude);
-
-    droneEntity.value = viewer.value.entities.add({
-      name: 'Drone',
-      position: initialPosition,
-      point: {
-        pixelSize: 10,
-        color: Color.RED,
-      },
-    });
-
-    viewer.value.zoomTo(droneEntity.value);
-  } else {
-    console.error("Les données JSON ne contiennent aucune position de drone valide.");
-  }
-});
-
-const playFlight = () => {
-  console.log('playFlight called');
-  if (currentIndex.value >= dronePath.value.length - 1) {
-    currentIndex.value = 0;
-  }
-
-  timer.value = setInterval(() => {
-    if (currentIndex.value < dronePath.value.length) {
-      updateDronePosition(dronePath.value[currentIndex.value]);
-      currentIndex.value++;
-    } else {
-      clearInterval(timer.value);
-    }
-  }, 1000);
-};
-
     const pauseFlight = () => {
-      console.log('pauseFlight called');
+      console.log('pauseFlight called')
       if (timer.value) {
-        clearInterval(timer.value);
-        timer.value = null;
+        clearInterval(timer.value)
+        timer.value = null
       }
     };
 
     const nextStep = () => {
-      console.log('nextStep called');
+      console.log('nextStep called')
       if (currentIndex.value < dronePath.value.length - 1) {
-        currentIndex.value++;
-        updateDronePosition(dronePath.value[currentIndex.value]);
+        currentIndex.value++
+        updateDronePosition(dronePath.value[currentIndex.value])
       }
     };
 
     const prevStep = () => {
-      console.log('prevStep called');
+      console.log('prevStep called')
       if (currentIndex.value > 0) {
-        currentIndex.value--;
-        updateDronePosition(dronePath.value[currentIndex.value]);
+        currentIndex.value--
+        updateDronePosition(dronePath.value[currentIndex.value])
       }
     };
 
     const jumpToStep = (index) => {
-      console.log('jumpToStep called with index:', index);
-      currentIndex.value = index;
-      updateDronePosition(dronePath.value[currentIndex.value]);
+      console.log('jumpToStep called with index:', index)
+      currentIndex.value = index
+      updateDronePosition(dronePath.value[currentIndex.value])
     };
 
     const updateDronePosition = (data) => {
-  console.log('updateDronePosition called with data:', data);
+      console.log('updateDronePosition called with data:', data)
 
-  if (!droneEntity.value) {
-    console.error("droneEntity is not défini.");
-    return;
-  }
+      if (!droneEntity.value) {
+        console.error("droneEntity is not défini.")
+        return;
+      }
 
-  const position = Cartesian3.fromDegrees(data.longitude, data.latitude, data.altitude);
-  droneEntity.value.position = position;
+      const position = Cartesian3.fromDegrees(data.longitude, data.latitude, data.altitude)
+      droneEntity.value.position = position
 
-  // Mettre à jour l'orientation du drone
-  const orientation = Transforms.headingPitchRollQuaternion(
-    position,
-    new HeadingPitchRoll(
-      CesiumMath.toRadians(data.attitude_head),
-      CesiumMath.toRadians(data.attitude_pitch),
-      CesiumMath.toRadians(data.attitude_roll)
-    )
-  );
+      // Mettre à jour l'orientation du drone
+      const orientation = Transforms.headingPitchRollQuaternion(
+        position,
+        new HeadingPitchRoll(
+          CesiumMath.toRadians(data.attitude_head),
+          CesiumMath.toRadians(data.attitude_pitch),
+          CesiumMath.toRadians(data.attitude_roll)
+        )
+      );
 
-  droneEntity.value.orientation = orientation;
-};
+      droneEntity.value.orientation = orientation
+    };
 
 
     return {
